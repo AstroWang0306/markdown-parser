9c15fc997267fb5c225adea006c62adb46c7279b
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-04-14 13:15:26 -0700

    Update MarkdownParse.java
    Fix- infinity loop by add a condition

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,19 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        //System.out.println(markdown.length());
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
+            int openParen = markdown.indexOf("(", closeBracket);    
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+
+            if(openBracket == -1){
+                break;
+            }
+            //System.out.println(currentIndex);
         }
 
         return toReturn;
@@ -25,9 +32,12 @@ public class MarkdownParse {
 
 
     public static void main(String[] args) throws IOException {
+        //System.out.println("hello");
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
     }
 }
+
+
