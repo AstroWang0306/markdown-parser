8d65f9d855680828380fc1d08d93dfde808d6876
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-04-21 13:15:59 -0700

    add TC for getLinks and run successfully

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
 
@@ -8,4 +12,18 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void test1 () throws IOException {
+
+        ArrayList<String> forTest1 = new ArrayList<String>();
+        forTest1.add("https://something.com");
+        forTest1.add("some-thing.html");
+        for(int i=0;i<2;i++) {
+            System.out.println(forTest1.get(i));
+        }
+
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(forTest1, MarkdownParse.getLinks(content));
+    }
 }
