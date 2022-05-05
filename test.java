5419ba378c6de75aed0fe47361fc37b139896c3b
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-05-05 12:35:33 -0700

    create test.java

--- test.java
+++ test.java
@@ -1,3 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class test{
-    //this a test file for testing 
+    //this a test file for testing makefile
+    @Test
+    public void aFailingtest() {
+        assertEquals("https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "Rick Roll");
+    }
 }
