005ba28949aa97d1055b482414055d2c9b7a1b6d
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-06-05 22:35:36 -0700

    update for MD

--- mdparse.sh
+++ mdparse.sh
@@ -1,1 +1,6 @@
-java -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar:. MarkdownParse $1
+#java -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar:. MarkdownParse $1
+for file in test-files/*.md;
+do
+    
+    java MarkdownParse $file
+done
