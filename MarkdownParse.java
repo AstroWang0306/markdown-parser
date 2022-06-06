4b7e8273778fc854bd48d897e1f7d59fcc8abf01
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-06-06 15:48:28 -0700

    update for markdown-parser

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,34 +15,46 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        //System.out.println(markdown.length());
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-             if(openBracket == nonexist){
-               break;
+            int backTick = markdown.indexOf("`",currentIndex);
+            if(backTick != nonexistent && backTick < openBracket) {
+                currentIndex = openBracket+1;
+                continue;
             }
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);    
-            int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen == nonexist){
+            if(openBracket == nonexistent) {
                 break;
             }
-            
-            String inParen = markdown.substring(openParen + 1, closeParen);
-            if(inParen.toLowerCase().endsWith(".jpg") || 
-                inParen.toLowerCase().endsWith(".png") ||
-                ! inParen.contains(".") || 
-                inParen.indexOf(".") == inParen.length()-1 
+            int closeBracket = markdown.indexOf("]", openBracket);
+            String checkLn = markdown.substring(openBracket,
+                closeBracket).replaceFirst("\n", "");
+            if(checkLn.contains("\n")) {
+                currentIndex = closeBracket+1;
+                continue;
+            }
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            if(closeParen == nonexistent) {
+                break;
+            }
+            String excludeBoundaryChars = markdown.substring(openParen+3,
+                closeParen-3);
+            if(excludeBoundaryChars.contains("\n")) {
+                currentIndex = closeParen+1;
+                continue;
+            }
+            String withinParen = markdown.substring(openParen+1,closeParen);
+            if(withinParen.toLowerCase().endsWith(".jpg") || 
+                withinParen.toLowerCase().endsWith(".png") ||
+                withinParen.toLowerCase().endsWith(".pdf") ||
+                ! withinParen.contains(".") || 
+                withinParen.indexOf(".") == withinParen.length()-1 
                 ) {}
             else {
-                toReturn.add(inParen);
+                toReturn.add(withinParen.trim());
             }
             currentIndex = closeParen + 1;
-
-            
-            //System.out.println(currentIndex);
         }
-
         return toReturn;
     }
 
